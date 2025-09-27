1daece7de52f482f15d28f0883638a7884471776
Philip Veremeichik <p.veremeichik@richbrains.net>
2025-09-27 10:41:29 +0300

    add profile

--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <TooltipProvider>
             <QueryClientProvider client={queryClient}>
                 <I18nextProvider i18n={i18n}>
-                    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+                    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
                         <RouterProvider router={router} />
                         <Toaster />
                     </ThemeProvider>
